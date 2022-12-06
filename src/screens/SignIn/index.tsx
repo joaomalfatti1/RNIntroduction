a2148d59dff824f52c7a1c8a888d5f6359b34cfc
joaomalfatti1 <joao.malfatti1210@gmail.com>
2022-12-06 19:20:18 -0400

    Finalizamos Tela de login, Splash, e etc

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,11 +1,50 @@
 import React from 'react';
-import { View, Text } from 'react-native';  
+import { 
+  View, 
+  Text,
+  Image,
+  StatusBar
+} from 'react-native';  
+
+import { ButtonIcon } from '../../components/ButtonIcon';
+import IllustrationImg from '../../assets/illustration.png';
+
 import { styles } from './styles';
 
 export  function SignIn(){
   return(
     <View style={styles.container}>
-      <Text> Hello World, tudo bem João Malfatti</Text>
+      <StatusBar 
+        barStyle='light-content'
+        translucent
+        backgroundColor={"transparent"}
+      />
+
+      <Image 
+        source={IllustrationImg}
+        style={styles.content}
+        resizeMode='stretch'
+      />
+
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Organize {`\n`}
+          suas jogatinas {`\n`}
+          facilmente
+        </Text>
+
+        <Text style={styles.subtitle}>
+          Crie grupos para jogar seus games {`\n`}
+          favoritos com seus amigos
+        </Text>
+
+        <ButtonIcon 
+          title= "Entrar com Discord"
+          activeOpacity={0.4}
+        />
+
+
+      </View>
     </View>
   )
 }
